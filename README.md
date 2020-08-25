96dbab9d35a8a87be69918b45c593967438d544b
Paul Hoekman <paul@penguinprotocols.com>
2020-08-25 12:15:10 +0200

    Setting up empty Docker repo 20

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+This contains the skeleton for the Stocks and Flows Database Schema (STAFdbs). This is kept in a separate repository from the Metabolism of Cities (MoC) website, as there are custom changes made that are specific to the MoC website that do not necessarily make sense in other contexts. This should therefore be considered a context-neutral version of the database schema.
---
+++ stafdbs/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stafdbs project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stafdbs.settings')
+
+application = get_asgi_application()
